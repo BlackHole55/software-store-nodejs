0e71d257457061f76ce7a837d433cb916b3ce8e7
Animantra <t20227893@gmail.com>
2026-02-06 00:11:18 +0500

    feat: user endpoints setup

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express from "express";
 
 // Repositories
 import { GameRepository } from "./infrastructure/mongoose/repositories/GameRepository.js";
+import { UserRepository } from "./infrastructure/mongoose/repositories/UserRepository.js";
 
 // Use Cases
 import { CreateGameUseCase } from "./usecases/game/CreateGame.js";
+import { CreateUserUseCase } from "./usecases/user/CreateUser.js";
 
 // Controllers
 import { GameController } from "./web/controllers/GameController.js";
+import { UserController} from "./web/controllers/UserController.js"; 
 
 // Routes
 import { gameRouter } from "./web/routes/gameRoutes.js";
+import { userRouter } from "./web/routes/userRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -22,14 +26,19 @@ app.use((req, res, next) => {
 
 // Initialize Repositories
 const gameRepo = new GameRepository();
+const userRepo = new UserRepository();
 
 // Initialize Use Cases
 const createGameUC = new CreateGameUseCase(gameRepo);
+const createUserUC = new CreateUserUseCase(userRepo);
 
 // Initialize Controllers
 const gameController = new GameController(createGameUC);
-
+const userController = new UserController(createUserUC)
 // Routes
 app.use('/api/v1/games', gameRouter(gameController));
+
+app.use('/api/v1/users', userRouter(userController));
+
 
 export default app;
