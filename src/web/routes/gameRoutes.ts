aba0c8070416dfe2e25d64b6391b3b8f7511811d
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-06 00:42:34 +0500

    feat: add authMiddleware to create route

--- src/web/routes/gameRoutes.ts
+++ src/web/routes/gameRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import type { GameController } from "../controllers/GameController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 export const gameRouter = (controller: GameController) => {
     const router = Router();
 
-    router.post('/', (req, res) => controller.handleCreate(req, res));
+    router.post('/', authMiddleware, (req, res) => controller.handleCreate(req, res));
 
     return router;
 }
