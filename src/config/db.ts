61eb4e758ef9ecd92d6969014e7ea699b26f03d7
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-05 19:41:10 +0500

    refactor: rewrite on typescript

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export const connectDB = async (): Promise<void> => {
+    try  {
+        const mongoUrl = process.env.MONGODB_CONNECT_URL;
+
+        if (!mongoUrl) {
+            throw new Error("MONGODB_CONNECT_URL is not defined in environment variables")
+        }
+
+        await mongoose.connect(mongoUrl);
+
+        console.log("MongoDB connected");
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error("MongoDB connection error:", err.message);
+        } else {
+            console.error("An unknown error occured during DB connection");
+        }
+
+        process.exit(1);
+    }
+};
