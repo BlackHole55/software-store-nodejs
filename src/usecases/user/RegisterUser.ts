23a2f69020e4c5e2e84257d8ad8a79c773b5104d
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-06 00:59:08 +0500

    feat: add password hashing and validate unique email

--- src/usecases/user/RegisterUser.ts
+++ src/usecases/user/RegisterUser.ts
@@ -1,12 +1,24 @@
 import type { IUserRepository } from "../../domain/repositories/IUserRepository.js";
 import type { User } from "../../domain/entities/User.js";
+import bcrypt from "bcryptjs";
 
-export class CreateUserUseCase {
+export class RegisterUserUseCase {
     constructor(private userRepo: IUserRepository) {}
 
     async execute(user: User): Promise<void> {
+        const existingUser = await this.userRepo.getByEmail(user.email);
+        if (existingUser) {
+            throw new Error("User with this email already exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(user.password_hash!, salt)
+
         user.created_at = new Date();
 
-        return await this.userRepo.create(user)
+        return await this.userRepo.create({
+            ...user,
+            password_hash: hashedPassword
+        });
     }
 }
