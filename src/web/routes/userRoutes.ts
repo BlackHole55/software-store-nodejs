0993cc80110b06279fdb6bdd4acf2b2cec5c0dae
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-06 01:21:29 +0500

    feat: change register route to signup

--- src/web/routes/userRoutes.ts
+++ src/web/routes/userRoutes.ts
@@ -4,7 +4,7 @@ import type { UserController } from "../controllers/UserController.js";
 export const userRouter = (controller: UserController) => {
     const router = Router();
 
-    router.post('/register', (req, res) => controller.handleRegister(req, res));
+    router.post('/signup', (req, res) => controller.handleRegister(req, res));
     router.post('/login', (req, res) => controller.handleLogin(req, res));
 
     return router;
