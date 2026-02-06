68293bdce31448ed74e8235b8d6a91a347419627
Animantra <t20227893@gmail.com>
2026-02-07 01:17:31 +0500

    feat: user getById usecase

--- src/usecases/company/GetById.js
+++ src/usecases/company/GetById.js
@@ -1,3 +1,14 @@
 export class GetByIdCompanyUseCase{
-    
+    constructor(companyRepo) {
+        this.companyRepo = companyRepo;
+    }
+
+    async execute(id) {
+        const company = await this.companyRepo.getById(id);
+        if (!company) {
+            throw new Error(`Company with ID ${id} not found`);
+        }
+
+        return company;
+    }
 }
