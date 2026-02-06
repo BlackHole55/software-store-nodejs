a1db2d4f33ff3fd28dcabbdead541ee0461f83a4
Animantra <t20227893@gmail.com>
2026-02-07 01:17:17 +0500

    feat: user getAllVerified usecase

--- src/usecases/company/GetAllVerified.js
+++ src/usecases/company/GetAllVerified.js
@@ -1,3 +1,15 @@
-export class GetAllVerifiedCompanyUseCase{
-    
+export class GetByIdCompanyUseCase{
+    constructor(companyRepo) {
+        this.companyRepo = companyRepo;
+    }
+
+    async execute() {
+        const company = await this.companyRepo.getById(id);
+       
+        if (!company || company.length == 0) {
+            throw new Error("No company found");
+        }
+
+        return company;
+    }
 }
