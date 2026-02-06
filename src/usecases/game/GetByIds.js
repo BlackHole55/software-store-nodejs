ea75042a1369b6d378fde53003f0cc6289ad43c9
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-06 22:25:42 +0500

    feat: add GetBYIds game use case

---
+++ src/usecases/game/GetByIds.js
@@ -0,0 +1,15 @@
+export class GetByIdGameUseCase {
+    constructor(gameRepo) {
+        this.game = gameRepo;
+    }
+
+    async execute(id) {
+        const games = await this.gameRepo.getByIds(id);
+
+        if (!games || games.length == 0) {
+            throw new Error("No games found");
+        }
+
+        return games;
+    }
+}
