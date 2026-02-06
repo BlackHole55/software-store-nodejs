996de3cd84f6ddb6a2a99337bf9df99800b13f43
Animantra <t20227893@gmail.com>
2026-02-07 02:59:49 +0500

    feat: user update repo

--- src/usecases/user/Update.js
+++ src/usecases/user/Update.js
@@ -1,26 +1,34 @@
+import bcrypt from "bcryptjs";
+
 export class UpdateUserUseCase {
     constructor(userRepo) {
         this.userRepo = userRepo;
     }
 
-    async execute(userId, updates) {
-        const existingUser = await this.userRepo.getById(userId);
+    async execute(id, updates, requestUserId, requestUserRole) {
+        const existingUser = await this.userRepo.getById(id);
         if (!existingUser) {
-            throw new Error(`User with ID ${userId} not found`);
+            throw new Error(`User with ID ${id} not found`);
         }
 
-        const allowedFields = ['name', 'email', '']; 
-        const filteredUpdates = Object.keys(updates)
-            .filter(key => allowedFields.includes(key))
-            .reduce((obj, key) => {
-                obj[key] = updates[key];
-                return obj;
-            }, {});
-
-        if (Object.keys(filteredUpdates).length === 0) {
-            return existingUser; 
+        if (requestUserRole !== "admin" && id !== requestUserId) {
+            throw new Error("Permission denied: you can only update your own profile");
         }
 
-        return await this.userRepo.update(userId, filteredUpdates);
+        const finalUpdateData = {
+            ...updates,
+            updated_at: new Date()
+        };
+
+        if (updates.password) {
+            const salt = await bcrypt.genSalt(10);
+            finalUpdateData.password = await bcrypt.hash(updates.password, salt);
+        }
+
+        if (updates.role && requestUserRole !== "admin") {
+            delete finalUpdateData.role;
+        }
+
+        return await this.userRepo.update(id, finalUpdateData);
     }
 }
