aeb3db724cf49175c4939430c7c0bb554483d76f
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-07 21:06:09 +0500

    refactor: game routes

--- src/web/routes/gameRoutes.js
+++ src/web/routes/gameRoutes.js
@@ -2,19 +2,28 @@ import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { validate } from "../middlewares/validate.js";
 import { createGameSchema, updateGameSchema } from "../schemas/gameSchema.js";
+import { roleMiddleware } from "../middlewares/roleMiddleware.js";
 
 export const gameRouter = (controller) => {
     const router = Router();
 
+    // Public routes
     router.get('/', controller.handleGetAllVerified);
-    router.get('/admin', authMiddleware, controller.handleGetAll);
-    router.get('/my-library', authMiddleware, controller.handleGetUserLibraryWithDetails);
     router.get('/:id', controller.handleGetById);
 
-    router.post('/', authMiddleware, validate(createGameSchema), controller.handleCreate);
-    router.put('/:id', authMiddleware, validate(updateGameSchema), controller.handleUpdate);
-    router.delete('/:id', authMiddleware, controller.handleDelete);
-    // router.delete('/:id', authMiddleware, controller.handleDelete);
+    // Aunthenticated users routes
+    const auth = Router();
+    auth.use(authMiddleware);
+
+    auth.get('/my-library', controller.handleGetUserLibraryWithDetails); // untested
+    auth.post('/', validate(createGameSchema), controller.handleCreate);
+    auth.put('/:id', validate(updateGameSchema), controller.handleUpdate);
+    auth.delete('/:id', controller.handleDelete);
+
+    // Admin routes
+    const admin = Router();
+    admin.use(roleMiddleware("admin"));
+    admin.get('/:id', controller.handleGetById);
 
     return router;
 }
