9e4a931c5bbae31256e60a81fa53ad7dd9c8450f
Animantra <t20227893@gmail.com>
2026-02-07 18:17:19 +0500

    feat: role middleware

---
+++ src/web/middlewares/roleMiddleware.js
@@ -0,0 +1,10 @@
+export const roleMiddleware = (requiredRole) => {
+    return (req, res, next) => {
+        if (!req.user || req.user.role !== requiredRole) {
+            return res.status(403).json({ 
+                error: "Forbidden: You don't have enough permissions" 
+            });
+        }
+        next();
+    };
+};
