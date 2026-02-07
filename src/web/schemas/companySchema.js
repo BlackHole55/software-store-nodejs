bc22a5ca75b0c0474ce19dbc8710bed688f707ad
Animantra <t20227893@gmail.com>
2026-02-07 18:15:29 +0500

    feat: add updated company schema

--- src/web/schemas/companySchema.js
+++ src/web/schemas/companySchema.js
@@ -1,20 +1,43 @@
 import { z } from 'zod';
-export const companySchema = z.object({
-        name: z.string(),
-        description: z.string().optional(),
-        country: z.string().optional(),
-        contacts: z.array(
-            z.object({
-                email: z.string(),
-                phone: z.string(),
-                website: z.string()
-            })
-        ).optional(),
-        is_verified: z.boolean().default(false),
-        library: z.array(
+export const createCompanySchema = z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    country: z.string().optional(),
+    contacts: z.array(
+        z.object({
+            email: z.string(),
+            phone: z.string(),
+            website: z.string()
+        })
+    ).optional(),
+    is_verified: z.boolean().default(false),
+    library: z.array(
+    z.object({
+        game_id: z.string(), 
+        added_at: z.string().datetime().optional(), 
+        playtime_hours: z.number().min(0).default(0)
+    })).optional()
+})
+
+
+export const updateCompanySchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    country: z.string().optional(),
+    
+    contacts: z.array(
+        z.object({
+            email: z.string().optional(),
+            phone: z.string().optional(),
+            website: z.string().optional()
+        })
+    ).optional(),
+
+    library: z.array(
         z.object({
             game_id: z.string(), 
             added_at: z.string().datetime().optional(), 
             playtime_hours: z.number().min(0).default(0)
-        })).optional()
-})
+        })
+    ).optional()
+});
