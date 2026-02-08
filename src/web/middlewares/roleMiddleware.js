86cc7253d7ec8b065b8811742bbfecb5052bd2e3
Animantra <t20227893@gmail.com>
2026-02-08 15:43:04 +0500

    fix: role middleware check role every time

--- src/web/middlewares/roleMiddleware.js
+++ src/web/middlewares/roleMiddleware.js
@@ -1,10 +1,23 @@
+import { UserModel } from "../../infrastructure/mongoose/models/UserModel.js";
+
 export const roleMiddleware = (requiredRole) => {
-    return (req, res, next) => {
-        if (!req.user || req.user.role !== requiredRole) {
-            return res.status(403).json({ 
-                error: "Forbidden: You don't have enough permissions" 
-            });
+    return async (req, res, next) => {
+        try {
+            if (!req.user || !req.user.id) {
+                return res.status(401).json({ error: "Unauthorized" });
+            }
+            const user = await UserModel.findById(req.user.id);
+
+            if (!user || user.role !== requiredRole) {
+                return res.status(403).json({ 
+                    error: "Forbidden: You don't have enough permissions" 
+                });
+            }
+
+            next();
+        } catch (error) {
+            console.error("Role Middleware Error:", error);
+            return res.status(500).json({ error: "Internal server error" });
         }
-        next();
     };
 };
