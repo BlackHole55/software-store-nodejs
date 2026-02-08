1956f99decd682b69703a701ddccafcaf3ae48c0
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-08 13:19:58 +0500

    fix: change createGameSchema to createPurchaseSchema

--- src/web/routes/purchaseRoutes.js
+++ src/web/routes/purchaseRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { validate } from "../middlewares/validate.js";
-import { purchaseSchema} from "../schemas/purchaseSchema.js";
+import { createPurchaseSchema } from "../schemas/purchaseSchema.js";
 import { roleMiddleware } from "../middlewares/roleMiddleware.js";
 
 export const gameRouter = (controller) => {
     const router = Router();
     router.use(authMiddleware);
 
-    router.post('/', validate(createGameSchema), controller.handleCreate);
+    router.post('/', validate(createPurchaseSchema), controller.handleCreate);
     router.get('/:id', controller.handleGetById);
     router.delete('/:id', controller.handleDelete);
 
