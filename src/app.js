06d7e485b809dfb234e1d21de061587b0e1ba499
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-08 15:40:38 +0500

    fix: add purchase routes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { initRegistry } from "./registry.js";
 import { gameRouter } from "./web/routes/gameRoutes.js";
 import { userRouter } from "./web/routes/userRoutes.js";
 import { companyRouter } from "./web/routes/companyRoutes.js";
+import { purchaseRouter } from "./web/routes/purchaseRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -14,10 +15,11 @@ app.use((req, res, next) => {
   next();
 });
 
-const { gameController, userController, companyController } = await initRegistry();
+const { gameController, userController, companyController, purchaseController } = await initRegistry();
 
 // Routes
 app.use('/api/v1/games', gameRouter(gameController));
 app.use('/api/v1/users', userRouter(userController));
 app.use('/api/v1/companies', companyRouter(companyController));
+app.use('/api/v1/purchases', purchaseRouter(purchaseController));
 export default app;
