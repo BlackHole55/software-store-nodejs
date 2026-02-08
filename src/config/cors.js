38b77d8d5098c5763e4f042e4511b3efee0f6f58
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-09 00:19:47 +0500

    fix: add cors

---
+++ src/config/cors.js
@@ -0,0 +1,21 @@
+import cors from 'cors';
+
+const allowedOrigins = [
+  'https://software-store-fronend-fork-production.up.railway.app',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  optionsSuccessStatus: 200
+};
+
+export default cors(corsOptions);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import app from './app.js'
+import corsMiddleware from './config/cors.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+app.use(corsMiddleware);
 
 async function start() {
     connectDB();
